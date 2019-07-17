eeeec9ee1ab03a229e2aefe940eab1ba00fc102d
Akash Roshan <akash.second1999@gmail.com>
2019-07-17 10:25:44 +0530

    renamed parts of project

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'forum-home'),
+    path('', views.boards, name = 'forum-board'),
     path('about/', views.about, name = 'forum-about')
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Board
 
-def home(request):
+def boards(request):
     context = {
         'boards': Board.objects.all()
     }
-    return render(request,'forum/home.html', context)
+    return render(request,'forum/boards.html', context)
 
 def about(request):
     return render(request, 'forum/about.html', context={'title':'About'})
