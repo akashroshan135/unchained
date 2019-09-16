c3b0aafd5eaf8a7110afe9c10c5b175ee90cc12a
Akash Roshan <akash.second1999@gmail.com>
2019-09-16 11:44:12 +0530

    added ability to delete files uploaded. Final update to branch

--- files/urls.py
+++ files/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('upload', views.upload, name = 'files-upload'),                    #the 'upload' function in the views.py is called when localhost is called
     path('view', views.file_view, name = 'files-view'),                     #the 'file_view' function in the views.py is called when localhost is called
+    path('view/delete-<pk>', views.delete_file, name = 'delete-file'),      #the 'delete_file' function in the views.py is called when localhost is called
 ]
 
 if settings.DEBUG:
--- files/views.py
+++ files/views.py
@@ -13,15 +13,24 @@ def upload(request):
             data = form.save(commit=False)                                                                      #saves the form in a variable
             data.user = request.user                                                                            #set 'user' field to current user logged in
             data.save()                                                                                         #saves the form in the database
-            return redirect('files-view')                                                                       #redirects to 'forum_threads' function with the forum's 'id' as args
+            messages.success(request, f"File has been uploaded successfully")                                   #sends a success message along with redirect to display in the page. Message block is stored in the 'base.html'
+            return redirect('files-view')                                                                       #redirects to 'files-view' view
         else:
-            messages.warning(request, f"No file was selected. Please select a file to upload")                  #sends a success message along with redirect to display in the login page. Message block is stored in the 'base.html'
-            form = NewFileForm()                                                                                #reloads form. Need to Add message integration
+            messages.warning(request, f"No file was selected. Please select a file to upload")                  #sends a warning message along with redirect to display in the page. Message block is stored in the 'base.html'
+            form = NewFileForm()                                                                                #reloads form
     return render(request, 'upload.html')                                                                       #request to render the upload.html file and sends the contex
 
 @login_required
 def file_view(request):
     context = {
-        'files'     :   File_Object.objects.filter(user=request.user)
+        'files'     :   File_Object.objects.filter(user=request.user)                                           #filters all the files uploaded by logged in user
     }
-    return render(request, 'file_list.html', context)
+    return render(request, 'file_list.html', context)                                                           #request to render the file_list.html file and sends the contex
+    
+@login_required
+def delete_file(request, pk):
+    if request.method == 'POST':                                                                                #if block is executed is the POST request is recieved.
+        file = File_Object.objects.get(pk=pk)                                                                   #get selected object and stores it in file variable
+        file.delete()                                                                                           #run delete function in models
+        messages.success(request, f"The file has been deleted")                                                 #show message
+    return redirect('files-view')                                                                               #redirects to 'files-view' view
